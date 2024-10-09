eaf6664f5ea67c232bfba4133257c0bca9214eca
jithts <jith.soman@aspiresys.com>
2024-10-09 19:22:31 +0530

    bug fix in routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
-import appIcon from '../icons/appIcon.png'
+import appIcon from "../icons/appIcon.png";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, UseDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { removerUser } from "../util/userSlice";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
-const dispatch= useDispatch()
-const navigate = useNavigate()
- const onClickLogoutButton  = () => {
+  const onClickLogoutButton = () => {
     dispatch(removerUser());
-    navigate('/')
- }
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="border-2 flex justify-between bg-lime-50">
       <div className="w-28 p-1">
@@ -19,23 +29,29 @@ const navigate = useNavigate()
       </div>
       <div className="flex items-center">
         <ul className="flex p-3 m-5 text-xl">
-          <li className="p-3"> Home
+          <li className="p-3">
+            {" "}
+            Home
             {/* <Link to="/">Home</Link> */}
           </li>
-          <li className="p-3"> About
+          <li className="p-3">
+            {" "}
+            About
             {/* <Link to="/about">About</Link> */}
           </li>
-          <li className="p-3"> List
+          <li className="p-3">
+            {" "}
+            List
             {/* <Link to="/Contact">Contact</Link> */}
           </li>
-          <li className="p-3"> create 
-            {/* <Link to="/groceory">gorceory </Link> */}
-          </li> 
           <li className="p-3">
-            <button onClick={onClickLogoutButton}>
-                log out
-            </button>
-            </li>  
+            {" "}
+            create
+            {/* <Link to="/groceory">gorceory </Link> */}
+          </li>
+          <li className="p-3">
+            <button onClick={onClickLogoutButton}>log out</button>
+          </li>
         </ul>
       </div>
     </div>
